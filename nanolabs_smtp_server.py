dc6d238ef546b7f58e516afca09533aff8e0d5a1
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2022-12-26 21:12:57 +0530

    update

--- nanolabs_smtp_server.py
+++ nanolabs_smtp_server.py
@@ -1,7 +1,6 @@
 def allow_or_notallow(userVal , line):
     domains = ["au" , "de" , "fr" ,"hk" , "id" ,"in" ,"jp" ,"lk","mm" ,"nz" ,"ru","uk" ,"us","vn"]
     topDomains = ["com" ,"dev","crew"]
-    temp = True
 
     if(len(userVal[0]) < 4 or len(userVal[0]) > 10 or len(userVal) != 2):
          return "BLOCKED"
@@ -9,16 +8,13 @@ def allow_or_notallow(userVal , line):
         for n in (userVal[0]):
             if(ord(n) < 48 or (ord(n) > 57 and ord(n) < 65) or (ord(n) > 90 and ord(n) < 97) or ord(n) > 122):
                 return "BLOCKED"
-                temp = False
-                break
-        if(temp):    
-            if(line[0] == "nanolabs" and line[1] in topDomains):
-                if((len(line) == 3 and line[2] in domains) or (len(line) == 2 and line[1] == "com")):
-                    return "ALLOWED"
-                else:
-                    return "BLOCKED"
+        if(line[0] == "nanolabs" and line[1] in topDomains):
+            if((len(line) == 3 and line[2] in domains) or (len(line) == 2 and line[1] == "com")):
+                return "ALLOWED"
             else:
                 return "BLOCKED"
+        else:
+            return "BLOCKED"
 
 val =  int(input())
 for _ in range(val):
