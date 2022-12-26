08b651be968b252d0cbd62f8086b12432df60575
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2022-12-26 17:44:06 +0530

    updates

---
+++ nanolabs_smtp_server.py
@@ -0,0 +1,27 @@
+def allow_or_notallow(userVal , line):
+    domains = ["au" , "de" , "fr" ,"hk" , "id" ,"in" ,"jp" ,"lk","mm" ,"nz" ,"ru","uk" ,"us","vn"]
+    topDomains = ["com" ,"dev","crew"]
+    temp = True
+
+    if(len(userVal[0]) < 4 or len(userVal[0]) > 10 or len(userVal) != 2):
+         return "BLOCKED"
+    else:
+        for n in (userVal[0]):
+            if(ord(n) < 48 or (ord(n) > 57 and ord(n) < 65) or (ord(n) > 90 and ord(n) < 97) or ord(n) > 122):
+                return "BLOCKED"
+                temp = False
+                break
+        if(temp):    
+            if(line[0] == "nanolabs" and line[1] in topDomains):
+                if((len(line) == 3 and line[2] in domains) or (len(line) == 2 and line[1] == "com")):
+                    return "ALLOWED"
+                else:
+                    return "BLOCKED"
+            else:
+                return "BLOCKED"
+
+val =  int(input())
+for _ in range(val):
+    userVal = input().split("@")
+    line = (userVal[1]).split(".")
+    print(allow_or_notallow(userVal , line))
